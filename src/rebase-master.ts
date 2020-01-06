0d6f2aced7def07ed6af6c000103606cea9dd01f
mp-intelligo <mpechthalt@intelligo.ai>
2020-01-06 15:04:44 +0200

    feature/ bug in rebase

--- src/rebase-master.ts
+++ src/rebase-master.ts
@@ -10,7 +10,7 @@ export default async () => {
 
     const content = await promises.readdir(process.cwd());
 
-    console.dir(content)
+    console.dir('CONTENT:', content)
 
     await git.init();
 
