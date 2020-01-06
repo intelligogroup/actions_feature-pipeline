c050e3b16da479c435d0bfc3cba78e3f91133bf9
mp-intelligo <mpechthalt@intelligo.ai>
2020-01-06 15:15:37 +0200

    log content

--- src/rebase-master.ts
+++ src/rebase-master.ts
@@ -10,7 +10,7 @@ export default async () => {
 
     const content = await promises.readdir(process.cwd());
 
-    console.dir('CONTENT:', content)
+    console.log('CONTENT:', content);
 
     await git.init();
 
