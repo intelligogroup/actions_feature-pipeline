dedf8a77afbbae657ea9ecf2c32dbc6372dc95e2
mp-intelligo <mpechthalt@intelligo.ai>
2020-02-04 14:34:42 +0200

    pull to update local branch before merge

--- src/merge-to.ts
+++ src/merge-to.ts
@@ -6,6 +6,7 @@ const { sourceBranch } = contextUtil;
 export default async (branchName: string, git: SimpleGit) => {
     try {
         await git.checkout(branchName);
+        await git.pull();
         await git.merge(['-Xignore-all-space', sourceBranch]);
         await git.push();
     } catch (error) {
