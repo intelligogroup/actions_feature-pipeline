66b3a1ebf32637e4cca76d96464628058a961c92
mp-intelligo <mpechthalt@intelligo.ai>
2020-03-30 12:06:14 +0300

    checkout sourceBranch

--- src/merge-to.ts
+++ src/merge-to.ts
@@ -9,6 +9,7 @@ export default async (branchName: string, git: SimpleGit) => {
     log(`🙈 Try merge ${sourceBranch} into ${branchName}...`);
 
     try {
+        await git.checkout(sourceBranch);
         await git.checkout(branchName);
         await git.pull();
         await git.merge(['-Xignore-all-space', sourceBranch]);
