4e79bb0e4c7581aad165048935bc9257d89a8d57
Omer Golan <94846645+ogolan-ig@users.noreply.github.com>
2024-01-09 16:46:45 +0200

    remove logs

--- src/index.ts
+++ src/index.ts
@@ -10,47 +10,35 @@ import mergeTo from './merge-to';
 
 
 (async () => {
-
-    console.log('check !!!!!!!!!!!')
     const { sha, message } = await getLastCommit(contextUtil.ref);
-    console.log(`latest commit message??: ${message}`);
+    console.log(`latest commit message: ${message}`);
 
     if (!message.includes('feature/')) {
         console.log('latest commit is not a ready feature. To trigger this action use the string "feature/" in the commit message');
         return;
     }
 
-    console.log('start !!!!!!!!!!!')
     const { sha: masterLastCommitSha } = await getLastCommit('refs/heads/master');
-    console.log('1 !!!!!!!!!!!!!!')
 
     if (sha === masterLastCommitSha) {
         console.log('Source and master branches are the same, no point in pull request!!');
         return;
     }
 
-    console.log('3 !!!!!!!!!!!!!!')
     const files = await compareToMaster();
-    console.log('4 !!!!!!!!!!!!!!')
 
     if (!files.length) {
         console.log('No file changes detected between source and master branches');
         return;
     }
-    console.log('5 !!!!!!!!!!!!!!')
     const git = await setupRepoFiles();
-    console.log('6 !!!!!!!!!!!!!!')
     // await rebaseMaster(git);
-    console.log('7 !!!!!!!!!!!!!!')
 
     await createPullRequest('master');
-    console.log('8 !!!!!!!!!!!!!!')
 
     await createPullRequest('stage');
-    console.log('9 !!!!!!!!!!!!!!')
 
     await mergeTo('stage', git);
-    console.log('10 !!!!!!!!!!!!!!')
 
     // if (!pull_number) {
     //     pull_number = await getLastPullRequestToStage();
