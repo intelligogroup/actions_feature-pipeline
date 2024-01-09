92c437c3d6f355b31414a124daa8630dddced6fa
Omer Golan <94846645+ogolan-ig@users.noreply.github.com>
2024-01-09 16:22:36 +0200

    more logs

--- src/compare-to-master.ts
+++ src/compare-to-master.ts
@@ -4,6 +4,7 @@ import contextUtil from "./context-util"
 const { owner, repo, sourceBranch } = contextUtil;
 
 export default async () => {
+    console.log(`Comparing ${sourceBranch} to master`);
     const { data: { files } } = await octokit.repos.compareCommits({
         owner,
         repo,
