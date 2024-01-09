d76a89d41b0783b713f9b32c0ba18afe25d5397c
Omer Golan <94846645+ogolan-ig@users.noreply.github.com>
2024-01-09 16:35:22 +0200

    more logs

--- src/context-util.ts
+++ src/context-util.ts
@@ -4,14 +4,17 @@ import { context } from '@actions/github';
 let repo: string, owner: string, ref: string, sha: string;
 
 if (process.env.IS_DEVELOPMENT_LOCAL_MODE) {
+    console.log('context if');
     repo = process.env.GIT_REPO;
     owner = process.env.GITHUB_OWNER;
     ref = process.env.GIT_REF;
     sha = process.env.GIT_SHA;
 } else {
+    console.log('context else');
     ({ repo: { repo, owner }, ref, sha } = context);
 }
 
+console.log('ref', ref);
 const sourceBranchName = ref.split('/')[2];
 
 if (!sourceBranchName || ['master', 'stage'].includes(sourceBranchName)) {
