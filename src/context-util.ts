8a88eae8d8e1e36165b5dcb90c4fdfa007f085e1
Omer Golan <94846645+ogolan-ig@users.noreply.github.com>
2024-01-09 16:43:18 +0200

    fix for ref branch

--- src/context-util.ts
+++ src/context-util.ts
@@ -4,18 +4,16 @@ import { context } from '@actions/github';
 let repo: string, owner: string, ref: string, sha: string;
 
 if (process.env.IS_DEVELOPMENT_LOCAL_MODE) {
-    console.log('context if');
     repo = process.env.GIT_REPO;
     owner = process.env.GITHUB_OWNER;
     ref = process.env.GIT_REF;
     sha = process.env.GIT_SHA;
 } else {
-    console.log('context else');
     ({ repo: { repo, owner }, ref, sha } = context);
 }
 
-console.log('ref', ref);
-const sourceBranchName = ref.split('/')[2];
+const splittedRefArray = ref.split('refs/heads/');
+const sourceBranchName = splittedRefArray[splittedRefArray.length - 1];
 
 if (!sourceBranchName || ['master', 'stage'].includes(sourceBranchName)) {
     throw new Error('This actions must be triggered from a branch other than stage and master');
